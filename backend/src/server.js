9cad8cd3aa13399c96c80aa33c298b9911a7269f
SHREYANSH80321 <SIBERSUPER1122@GMAIL.com>
2025-08-22 02:19:40 +0530

    auth implemented and DB connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,14 @@ import express from "express";
 import dotenv from "dotenv/config"
 import {ENV} from "./config/env.js"
 import { connectDB } from "./config/db.js";
+import {clerkMiddleware, Client} from "@clerk/express"
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 const app = express();
+app.use(express.json());
+app.use(clerkMiddleware())
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hello World 1 2 3")
@@ -10,7 +17,20 @@ app.get("/", (req, res) => {
   
 })
 
-app.listen(ENV.PORT, () => {
-  console.log("Server started on port", ENV.PORT)
-  connectDB();
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port", ENV.PORT);
+      });
+      
+    }
+  } catch (error) {
+    console.log("Error starting server");
+    process.exit(1);
+    
+  }
+}
+startServer();
+export default app;
