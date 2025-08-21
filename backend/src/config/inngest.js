13988a0540f0e81c23f14d898fa4b34748d377fa
SHREYANSH80321 <SIBERSUPER1122@GMAIL.com>
2025-08-22 03:20:06 +0530

    new fix for mongo db

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import {User} from "../models/user.model.js"
+import { connectDB } from "./db.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-app" });
 
