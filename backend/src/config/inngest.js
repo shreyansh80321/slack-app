e24254e9dcf18fbcad89a2853d098b3ed1a15af0
SHREYANSH80321 <SIBERSUPER1122@GMAIL.com>
2025-08-22 12:25:37 +0530

    stream & sentry added

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import {User} from "../models/user.model.js"
 import { connectDB } from "./db.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-app" });
 
@@ -19,6 +20,11 @@ const syncUser = inngest.createFunction(
       image:image_url,
     }
     await User.create(newUser)
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.image,
+    })
   }
 );
 
@@ -28,6 +34,8 @@ const deleteUserFromDB = inngest.createFunction(
   async ({ event }) => {
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
 
   }
 )
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,6 @@
+export const protectRoute = (req, res, next) => {
+  if (!req.auth, isAuthenticated) {
+    return res.status(401).json({message:"Unuthorized - you must be logged in"})
+  }
+  next();
+}
